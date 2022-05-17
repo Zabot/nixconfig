e330f2ea29401b78adfd53642a2bacbee5d170dd
Zach Anderson <zach.inbox@gmail.com>
2022-05-16 20:32:00 -0500

    Add xps machine definition

---
+++ machines/xps/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./boot.nix
+  ];
+
+  networking.wireless.interfaces = [ "wlp0s20f3" ];
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  # Slight perf boost and save some SSD write cycles by not updating atime
+  # Send TRIM commands to the ssd when blocks are freed
+  fileSystems."/".options = ["noatime" "nodiratime" "discard" ];
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Enable fingerprint auth
+  hardware.defaultWifi = "wlp0s20f3";
+  hardware.power = {
+    battery = "BAT0";
+    adapter = "AC";
+  };
+}
