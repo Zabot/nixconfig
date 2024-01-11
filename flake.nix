8d293dcf1f182e230943e92cf68943e284c1edac
Zach Anderson <zach@zabot.dev>
2024-01-10 19:29:37 -0600

    correct

--- flake.nix
+++ flake.nix
@@ -3,36 +3,39 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, home-manager }: let
-    user = {
-      unixname = "zach";
-      name = "Zach Anderson";
-      email = "zach@zabot.dev";
-    };
-  in {
-    nixosConfigurations.zach-xps = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-      specialArgs = {
-        inherit home-manager;
-        global = {
-          inherit user;
-          host = "xps";
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      user = {
+        unixname = "zach";
+        name = "Zach Anderson";
+        email = "zach@zabot.dev";
+      };
+    in
+    {
+      nixosConfigurations.zach-xps = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit home-manager;
+          global = {
+            inherit user;
+            host = "xps";
+          };
         };
       };
-    };
-    nixosConfigurations.zach-framework = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-      specialArgs = {
-        inherit home-manager;
-        global = {
-          inherit user;
-          host = "framework";
+      nixosConfigurations.zach-framework = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit home-manager;
+          global = {
+            inherit user;
+            host = "framework";
+          };
         };
       };
+      homeConfigurations.default = home-manager.lib.homeManagerConfiguration
+        import ./home/home-manager.nix;
     };
-    homeConfigurations.default = import ./home/home-manager.nix;
-  };
 }
 
