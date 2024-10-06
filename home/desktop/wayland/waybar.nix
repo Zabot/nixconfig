99b51664ecf7ace4f5164df55c476ab62bc266b7
Zach Anderson <zach@zabot.dev>
2024-10-06 02:31:58 -0500

    Option for wayland

---
+++ home/desktop/wayland/waybar.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, system, ... }:
+{
+  programs.waybar = lib.mkIf config.desktop.useWayland {
+    enable = true;
+    systemd = {
+      enable = true;
+    };
+
+    settings = rec {
+      "bar/primary" = {
+        position = "bottom";
+
+        modules-left = [ "sway/workspaces" ];
+        modules-center = [ "mpd" ];
+        modules-right = [ "pulseaudio" "network" "battery" "clock" ];
+      };
+    };
+  };
+}
