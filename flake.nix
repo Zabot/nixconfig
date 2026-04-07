ccf80c887bef2afd0f255006b1a8b4ac6fa552b2
Zach Anderson <zach@zabot.dev>
2026-04-07 02:58:24 -0500

    unfollow everything

--- flake.nix
+++ flake.nix
@@ -1,26 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fel.url = "github:zabot/fel";
     agenix.url = "github:ryantm/agenix";
+    disko.url = "github:nix-community/disko/latest";
+    fel.url = "github:zabot/fel";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nur.url = "github:nix-community/NUR";
   };
 
   outputs =
